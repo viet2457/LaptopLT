16cded414169a000afa9fc64bc97c82b63b1eb2a
Viet1234 <kudo91981@gmail.comgit config --global user.name Viet1234>
2023-05-22 20:10:13 +0700

    laptop

---
+++ LaptopLT/laptoplt/resources/views/pages/contact/contact.blade.php
@@ -0,0 +1,34 @@
+@extends('layout')
+@section('content')
+
+<div class="wrap-categories">
+    <div class="container mb-4">
+        <div class="product-contact">
+                <div class="text-center mb-3">
+                    <div style="color: #fff;" class="title-content">
+                        <img src="{{URL('public/client/img/dot-title-left.png')}}" alt=""> Thông tin liên hệ <img src="{{URL('public/client/img/dot-title-right.png')}}" alt="">
+                        <hr>
+                        <p align="justify" style="font-size:14px; color:#fff;">Laptop LT là một trong những doanh nghiệp hàng đầu hoạt động trong lĩnh vực kinh doanh các sản phẩm CNTT. Thành lập từ năm 2020 đến nay, Công ty đã tạo được chỗ đứng vững chắc trên thị trường bán buôn, bán lẻ, trở thành thương hiệu quen thuộc và là đối tác tin cậy của nhiều bạn hàng trong nước và Quốc tế. Luôn lấy yếu tố hài hòa về lợi ích làm nền tảng, lãnh đạo Công ty hiểu rằng, niềm tin của khách hàng về giá thành, chất lượng và dịch vụ là sự sống còn của Công ty. Do vậy, mọi hoạt động kinh doanh của Công ty luôn hướng tới mục tiêu tôn trọng và bảo đảm quyền lợi cho khách hàng, chinh phục khách hàng bằng chất lượng sản phẩm và dịch vụ tối ưu. Để được phục vụ tốt nhất, Quý khách vui lòng liên hệ với chúng tôi theo các thông tin sau:</p>
+                        <hr>
+                    </div>
+                </div>
+                <div class="pro-contact-content">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-6">
+                            <p style="font-size: 20px;" class="title-content">Laptop LT</p>
+                            <p>Địa chỉ: <span><a href="https://goo.gl/maps/g7GaF8ViwoCjwUdW6">470 Trần Đại Nghĩa, Q. Ngũ Hành Sơn, Tp. Đà Nẵng</a></span></p>
+                            <p>Hotline: <span><a href="tel: 037 855 6163">037 855 6163 -</a><a href="tel: 0916 190 744"> 0916 190 744</a></span></p>
+                            <p>Email: <span><a href="mailto: pvlinh.20it9@vku.udn.vn">pvlinh.20it9@vku.udn.vn</a></span></p>
+                            <p>Fanpage: <span><a href="https://www.facebook.com/laptoplt/">Công ty Laptop LT</a></span></p>
+                            <p>Website: <span><a href="#">Laptop LT</a></span></p>
+                        </div>
+                        <div class="col-sm-12 col-md-6 text-center">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3143.8845962337627!2d108.25107529333566!3d15.97470262095603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142108997dc971f%3A0x1295cb3d313469c9!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2jhu4cgVGjDtG5nIHRpbiB2w6AgVHJ1eeG7gW4gdGjDtG5nIFZp4buHdCAtIEjDoG4!5e1!3m2!1svi!2s!4v1634551700603!5m2!1svi!2s"
+                                width="90%" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+@endsection
